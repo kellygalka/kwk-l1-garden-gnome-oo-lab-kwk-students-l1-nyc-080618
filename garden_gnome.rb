6d11b4bcc58458665543039c1f3387f16d13b7ab
kellygalka <kelly3000a1@gmail.com>
2018-08-10 16:41:09 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,23 @@
 # Code your instances here
+class GardenGnome
+  
+  def initialize
+    @name = "Carl the Crappy"
+    @age = 3432
+  end
+  
+  def name=(name)
+  end
+  
+  def name
+    @name
+  end
+  
+  def age=(age)
+  end
+  
+  def age
+    @age
+  end
+  
+end
